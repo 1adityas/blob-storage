28a050a6e0234f15f7b3ad5c4aa710e8d6f6c69c
aditya pratap singh <1adityapsingh@gmail.com>
2023-09-05 04:19:36 +0530

    upload blob

--- blobStorage-ConsoleApp/Program.cs
+++ blobStorage-ConsoleApp/Program.cs
@@ -4,7 +4,16 @@ using System;
 using System.IO;
 using Azure.Identity;
 using blobStorage_ConsoleApp;
+using Nito.AsyncEx;
+
+string connectionString = constants.connectionString;
+var blobCrudObject = new blob_crud(connectionString);
+
+//to create a new container
+
+AsyncContext.Run(blobCrudObject.NewContainer);
+
+AsyncContext.Run(blobCrudObject.UploadBlob);
 
 
-string connectionString= constants.connectionString;
 
--- blobStorage-ConsoleApp/blob-crud.cs
+++ blobStorage-ConsoleApp/blob-crud.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,38 +14,56 @@ namespace blobStorage_ConsoleApp
         private string connectionString { get; set; }
         public blob_crud(string connectionString)
         {
-            this.connectionString=connectionString;
+            this.connectionString = connectionString;
 
         }
+
+        public async void NewContainer()
+        {
+            var blobServiceClient = new BlobServiceClient(connectionString);
+            //Create a unique name for the container
+            string containerName = "quickstartblobs" + Guid.NewGuid().ToString();
+
+            // Create the container and return a container client object
+            BlobContainerClient containerClient = await blobServiceClient.CreateBlobContainerAsync(containerName);
+            //Console.WriteLine(containerClient);
+
+        }
+        public async void UploadBlob()
+        {
+
+            // Create a local file in the ./data/ directory for uploading and downloading
+            string localPath = "/data";
+            Directory.CreateDirectory(localPath);
+            string fileName = "quickstart" + Guid.NewGuid().ToString() + ".txt";
+            string localFilePath = Path.Combine(localPath, fileName);
+            // Write text to the file
+            await File.WriteAllTextAsync(localFilePath, "Hello, World!");
+
+            // Get a reference to a blob
+            BlobClient blobClient = new BlobClient(connectionString, "newContainer-1", fileName);
+            await blobClient.UploadAsync(localFilePath, true);
+        }
+        //public async void UploadBlob()
+        //{
+
+        //    // Create a local file in the ./data/ directory for uploading and downloading
+        //    string localPath = "data";
+        //    Directory.CreateDirectory(localPath);
+        //    string fileName = "quickstart" + Guid.NewGuid().ToString() + ".txt";
+        //    string localFilePath = Path.Combine(localPath, fileName);
+        //    // Write text to the file
+        //    await File.WriteAllTextAsync(localFilePath, "Hello, World!");
+
+        //    // Get a reference to a blob
+        //    BlobClient blobClient = new BlobClient(connectionString, "newContainer-1", fileName);
+        //    await blobClient.UploadAsync(localFilePath, true);
+        //}
+
+
 
 
 
 
     }
-
-    var blobServiceClient = new BlobServiceClient(connectionString);
-
-    //Create a unique name for the container
-    string containerName = "quickstartblobs" + Guid.NewGuid().ToString();
-
-    // Create the container and return a container client object
-    BlobContainerClient containerClient = await blobServiceClient.CreateBlobContainerAsync(containerName);
-    //Console.WriteLine(containerClient);
-
-    // Create a local file in the ./data/ directory for uploading and downloading
-    string localPath = "data";
-    Directory.CreateDirectory(localPath);
-string fileName = "quickstart" + Guid.NewGuid().ToString() + ".txt";
-    string localFilePath = Path.Combine(localPath, fileName);
-
-    // Write text to the file
-    await File.WriteAllTextAsync(localFilePath, "Hello, World!");
-
-    // Get a reference to a blob
-    BlobClient blobClient = containerClient.GetBlobClient(fileName);
-
-    Console.WriteLine("Uploading to Blob storage as blob:\n\t {0}\n", blobClient.Uri);
-
-// Upload data from the local file
-await blobClient.UploadAsync(localFilePath, true);
 }
